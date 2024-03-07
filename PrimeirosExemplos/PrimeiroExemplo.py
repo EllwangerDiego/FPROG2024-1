3d356f7a38f8a257e72d15b57bcc471d916d9ac7
EllwangerDiego <162519626+EllwangerDiego@users.noreply.github.com>
2024-03-06 21:39:45 -0300

    Arquivo PrimeiroExemplo.py criado

---
+++ PrimeirosExemplos/PrimeiroExemplo.py
@@ -0,0 +1,25 @@
+#Comando de Saída
+print('Hello!')
+
+#Criando uma variavel numérica e atribuindo um valor
+num = 10
+
+# Imprimindo o valor da variavel num
+print (num)
+
+#trocando o valor da variável num
+num = -1
+
+print (num)
+
+#Leitura (comando de entrada) de valor para a variavel nome
+nome = input('Digite seu nome e pressione ENTER: ')
+
+print ('Olá',nome)
+
+#Leitura de valor inteiro para a variavel idade
+idade = int(input('Digite sua idade: '))
+
+print ('Sua idade é:', idade)
+
+print('Seu nome é', nome, 'e sua idade é ',idade)
---
+++ PrimeirosExemplos/teste.py
@@ -0,0 +1,11 @@
+print ('Boa Noite')
+
+nome = input('Digite seu nome: ')
+
+print ('Boa noite,', nome)
+
+idade = int(input('Digite sua idade: '))
+
+print('Sua idade é: ',idade)
+
+print('Seu nome é:', nome, 'e sua idade é: ',idade)
