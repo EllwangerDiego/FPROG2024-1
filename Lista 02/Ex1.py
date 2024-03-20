4b3f03471b4e110494ee7ce8d84b98a31692d9da
Diego Ellwanger <ellwangerdiego@gmail.com>
2024-03-20 19:36:25 -0300

    Arrumei mais alguns erros

--- Lista 02/Ex1.py
+++ Lista 02/Ex1.py
@@ -1,5 +1,8 @@
+print('')
 minutos = int(input('Digite o tempo em minutos: '))
+print('')
 
 segundos = minutos * 60
-
+print('')
 print (minutos, 'minutos equivalem a', segundos, 'segundos')
+print('')
---
+++ Lista 02/Ex3.py
@@ -0,0 +1,9 @@
+#3. Um restaurante de buffet a quilo cobra R$ 40,00 por quilo. Escreva um programa que leia o peso
+#do prato do cliente e calcule o valor a ser pago.
+
+peso = float(input('Digite aqui o peso do seu prato em KG: '))
+
+ValorFinal = peso * 40
+print('')
+print('Seu prato custará: R$', ValorFinal,)
+print('')
