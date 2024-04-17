6444504d8a5cc4874567b742cceb643c48a2b27b
Diego Ellwanger <ellwangerdiego@gmail.com>
2024-04-17 20:50:25 -0300

    14

--- Exemplos/Corrida.py
+++ Exemplos/Corrida.py
@@ -1,75 +1,78 @@
 import random
 import time
 import os
-p1 = 0
-p2 = 0
 
-vencedor = 0
+def movimentarHamster(posHamster):
+        
+    p1 = 0
+    p2 = 0
 
-while vencedor == 0:
-    time.sleep(0.1)
-    os.system ('cls' if os.name == 'nt' else 'clear')
+    vencedor = 0
 
-    nroSorteado = random.randint (1,5)
-    if nroSorteado == 1:
-        p1 = p1 + 1
-    elif nroSorteado == 2:
-        p1 = p1 + 2
-    elif nroSorteado == 3:
-        pass
-    elif nroSorteado == 4:
-        p1 = p1 -1
-    else:
-        p1 = p1 - 2
-    if p1 < 0:
-        p1 = 0
-    if p1 > 12:
-        p1 = 12
+    while vencedor == 0:
+        time.sleep(0.1)
+        os.system ('cls' if os.name == 'nt' else 'clear')
 
-        #-------------------------------------------------------------------------------------------------#
-       
         nroSorteado = random.randint (1,5)
-    if nroSorteado == 1:
-        p2 = p2 + 1
-    elif nroSorteado == 2:
-        p2 = p2 + 2
-    elif nroSorteado == 3:
-        pass
-    elif nroSorteado == 4:
-        p2 = p2 -1
-    else:
-        p2 = p2 - 2
-    if p2 < 0:
-        p2 = 0
-    if p2 > 12:
-        p2 = 12
-
-    if p1 == 12:
-        vencedor = 1
-    if p2 == 12:
-        if vencedor == 0:
-            vencedor = 2
+        if nroSorteado == 1:
+            p1 = p1 + 1
+        elif nroSorteado == 2:
+            p1 = p1 + 2
+        elif nroSorteado == 3:
+            pass
+        elif nroSorteado == 4:
+            p1 = p1 -1
         else:
-            vencedor = 3
+            p1 = p1 - 2
+        if p1 < 0:
+            p1 = 0
+        if p1 > 12:
+            p1 = 12
 
-    print('Hamster 1: ')
-    for n in range(p1):
-        print('*', end = '')
-    print()
+            #-------------------------------------------------------------------------------------------------#
+        
+            nroSorteado = random.randint (1,5)
+        if nroSorteado == 1:
+            p2 = p2 + 1
+        elif nroSorteado == 2:
+            p2 = p2 + 2
+        elif nroSorteado == 3:
+            pass
+        elif nroSorteado == 4:
+            p2 = p2 -1
+        else:
+            p2 = p2 - 2
+        if p2 < 0:
+            p2 = 0
+        if p2 > 12:
+            p2 = 12
 
-    print('Hamster 2: ')
-    for n in range(p2):
-        print('*', end = '')
-    print()
+        if p1 == 12:
+            vencedor = 1
+        if p2 == 12:
+            if vencedor == 0:
+                vencedor = 2
+            else:
+                vencedor = 3
 
-if vencedor == 1:
-    print('Hamster 1 venceu')
-    print()
-elif vencedor == 2:
-    print('Hamster 2 venceu')
-    print()
-else:
-    print('A corrida empatou')
-    print()
-    
+        print('Hamster 1: ')
+        for n in range(p1):
+            print('*', end = '')
+        print()
+
+        print('Hamster 2: ')
+        for n in range(p2):
+            print('*', end = '')
+        print()
+
+    if vencedor == 1:
+        print('Hamster 1 venceu')
+        print()
+    elif vencedor == 2:
+        print('Hamster 2 venceu')
+        print()
+    else:
+        print('A corrida empatou')
+        print()
+        
     
--- Lista 03/Ex14.py
+++ Lista 03/Ex14.py
@@ -7,4 +7,31 @@
 # d) dependentes com mais de 60 anos pagam R$ 480.
 
 print()
+valor1 = 300
+criancas = 100
+d1 = 220
+d2 = 395
+d3 = 480
 
+print()
+idade = float(input("Digite aqui a sua idade: "))
+print()
+
+if idade < 10:
+    print("O valor do seu plano de saúde é de: ", criancas)
+    print()
+
+elif idade > 9 and idade < 31:
+    valorF = valor1 + d1
+    print("O valor do seu plano de saúde é de: ", valorF )
+    print()
+
+elif idade > 30 and idade < 61:
+    valorF = valor1 + d2
+    print("O valor do seu plano de saúde é de: ", valorF )
+    print()
+
+elif idade > 60:
+    valorF = valor1 + d3
+    print("O valor do seu plano de saúde é de: ", valorF )
+    print()
